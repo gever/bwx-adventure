6ded3efeb6c00900cc67a1132da25cac1afc4c11
Andrew Lambeth <w.andrew.lambeth@gmail.com>
2015-01-16 12:04:48 -0800

    succeed if at least one of the nouns was verbed

--- advent.py
+++ advent.py
@@ -281,10 +281,11 @@ class BaseVerb(Base):
       result = False
     # treat 'verb noun1 and noun2..' as 'verb noun1' then 'verb noun2'
     # treat 'verb noun1, noun2...' as 'verb noun1' then 'verb noun2'
+    # if any of the nouns work on the verb consider the command successful,
+    # even if some of them don't
     if words:
       for noun in words:
-        if not self.function(actor, noun, None):
-          result = False
+        result |= self.function(actor, noun, None)
     return result
 
 class Say(BaseVerb):
