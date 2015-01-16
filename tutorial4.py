317867ed39eeb099f02d073e5084e34d4ab393d8
Andrew Lambeth <w.andrew.lambeth@gmail.com>
2015-01-16 00:19:21 -0800

    add some explanatory comments about Food and Drink

--- tutorial4.py
+++ tutorial4.py
@@ -37,11 +37,23 @@ key = sidewalk.new_object("key", "a small tarnished key")
 
 player = game.new_player(sidewalk)
 
+#
+# We have a special class of Object called "Food" that lets you easily create
+# things that can be eaten.
+#   - the first argument is the name of the object containing or holding the food
+#   - the second argument is a description of the food
+#   - the third argument is any kind of Verb that you want to execute
+#     when the food is consumed
+# Try using the "eat" command on the plate created below.
+# Try using it a second time and see what happens.
+#
 office.add_object(Food("plate",
                        "plate of green eggs and ham.",
                        Say("You like them!")))
 
-                                                
+#
+# Drink is another special class just like Food, but for things you can drink.
+#                                               
 office.add_object(Drink("glass",
                         "a glass of water, half full",
                         Say("You feel refreshed, and strangely optimistic")))
