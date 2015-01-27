6243b5b916a84671cf80f8539b8a1b38707cac98
Andrew Lambeth <w.andrew.lambeth@gmail.com>
2015-01-26 19:40:06 -0800

    more examples of death, including fighting

--- tutorial7.py
+++ tutorial7.py
@@ -57,6 +57,45 @@ sidewalk.add_object(Drink("vial",
                           Die("choking violently and collapsing onto the floor..."),
                           Object("empty vial", "an empty vial with an acrid odor")))
 
+#
+# Now lets add an unbeatable foe, better to just to leave this guy alone!
+#
+# We use the add_phrase() method to link the Die verb to the phrase "wake bear".
+# Now any actor that utters that phrase near the bear will be killed.
+# 
+# See what happens if you do:
+# > wake bear
+#
+bear = Animal("sleeping bear")
+bear.set_location(vestibule)
+bear.set_allowed_locations([vestibule])
+game.add_actor(bear)
+bear.add_phrase("wake bear",
+                Die("mauled viciously by the angry bear who then falls back asleep."))  
+
+#
+# Let's try a more complex example now that involves fighting.
+# We create a function, "fight_dragon" that checks if we have the shield or not.
+# If we do have the shield then we can kill the dragon, if not he will kill us.
+# The fight_dragon() function uses the Actor.terminate() method to kill whichever
+# actor loses the battle.
+# We use add_phrase to link the phrase "fight dragon" to the fight_dragon function.
+# We add the phrase to the dragon so that it only works when the dragon is present.
+#
+dragon = Actor("tiny dragon")
+dragon.set_location(office)
+game.add_actor(dragon)
+shield = vestibule.new_object("shield", "a shiny bronze sheild")
+sword = office.new_object("sword", "a rusty old sword")
+def fight_dragon(game, thing):
+  if not "shield" in game.player.inventory:
+    game.output("You try to stab the dragon with the sword, but it flames you.")
+    player.terminate()
+  else:
+    game.output("Using the shield to avoid the dragon's flames you kill it with the sword.")
+    dragon.terminate()
+
+dragon.add_phrase("fight dragon", fight_dragon)
             
 # add our player to the game
 game.add_actor(player)
