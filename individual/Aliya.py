888197800dd102dedb1a1c8f41a8efafe35048df
sleepinghungry <WWCLC@asdconnect.org>
2017-01-10 14:36:37 -0800

    Added shovel

--- individual/Aliya.py
+++ individual/Aliya.py
@@ -18,9 +18,15 @@ up_tree = game.new_location(
   "Up Tree",
 """You are now up a tree and overlooking a big forest and you are in the middle of it""")
 
+river = game.new_location(
+  "River",
+"""You are next to a river. There is a shovel next to the river""")
 
+shovel = river.new_object("shovel", "a steel shovel")
+ 
+  
 game.new_connection("Glass Door", forest, up_tree, [UP], [DOWN])
-
+game.new_connection("Glass Door", forest, river, [EAST], [WEST])
 player = game.new_player(forest)
 
 game.run()
