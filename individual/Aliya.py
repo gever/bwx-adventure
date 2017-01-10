61154a6b04d6b26c4a8aaf256660972e5b0208db
sleepinghungry <WWCLC@asdconnect.org>
2017-01-10 14:17:48 -0800

    Aliya's Game

---
+++ individual/Aliya.py
@@ -0,0 +1,26 @@
+#!/user/bin/python
+# vim: et sw=2 ts=2 sts=2
+#
+import os
+import sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from bwx_adventure.advent import *
+from bwx_adventure.advent import Game, Location, Connection, Object, Animal, Robot, Pet, Player
+from bwx_adventure.advent import NORTH, SOUTH, EAST, WEST, UP, DOWN, RIGHT, LEFT, IN, OUT, FORWARD, BACK, NORTH_WEST, NORTH_EAST, SOUTH_WEST, SOUTH_EAST, NOT_DIRECTION
+
+game = Game("Lucky Day")
+
+forest = game.new_location(
+  "Forest",
+  "A lush green forest stands before you with tall trees")
+
+up_tree = game.new_location(
+  "Up Tree",
+"""You are now up a tree and overlooking a big forest and you are in the middle of it""")
+
+
+game.new_connection("Glass Door", forest, up_tree, [UP], [DOWN])
+
+player = game.new_player(forest)
+
+game.run()
